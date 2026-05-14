e973dca9e5f772f9be52985d735673ba0859ab19
epreinhad <epreinhad@gmail.com>
2026-05-15 01:14:25 +0700

    calculator gaji
    calculator gaji sederhana basis java

---
+++ kalkulatorgaji.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class kalkulatorgaji {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String golongan;
+        int jamLembur;
+
+        double gajiPokok = 0;
+        double gajiLembur = 0;
+        double totalGaji;
+
+        System.out.print("Masukkan Golongan Karyawan (A/B/C): ");
+        golongan = input.nextLine();
+
+        System.out.print("Masukkan Jam Lembur: ");
+        jamLembur = input.nextInt();
+
+        if (golongan.equalsIgnoreCase("A")) {
+            gajiPokok = 5000000;
+        } else if (golongan.equalsIgnoreCase("B")) {
+            gajiPokok = 6500000;
+        } else if (golongan.equalsIgnoreCase("C")) {
+            gajiPokok = 9500000;
+        } else {
+            System.out.println("Golongan tidak valid");
+        }
+        
+        
+        if (jamLembur == 1) {
+            gajiLembur = 0.30 * gajiPokok;
+        } else if (jamLembur == 2) {
+            gajiLembur = 0.32 * gajiPokok;
+        } else if (jamLembur == 3) {
+            gajiLembur = 0.34 * gajiPokok;
+        } else if (jamLembur == 4) {
+            gajiLembur = 0.36 * gajiPokok;
+        } else if (jamLembur >= 5) {
+            gajiLembur = 0.38 * gajiPokok;
+        } else {
+            gajiLembur = 0;
+        }
+
+        totalGaji = gajiPokok + gajiLembur;
+
+        System.out.println("Gaji Pokok     : Rp " + gajiPokok);
+        System.out.println("Gaji Lembur    : Rp " + gajiLembur);
+        System.out.println("Total Gaji     : Rp " + totalGaji);
+
+    }
+}
